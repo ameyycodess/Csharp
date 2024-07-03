7839a0c8f7c707a88addd2ba5a2fc45ba647b2c1
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-07-03 23:31:21 +0530

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Sum
+{
+    public static void Main(string[] args)
+    {
+        int m = 100;
+        long n = 200;
+        long l = m + n;
+        int l1 = (int)l;
+        Console.WriteLine(l1);
+    }
+}
