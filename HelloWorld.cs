074f34dbbc122126b52df4980509df35cde8df91
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-07-03 22:13:07 +0530

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,13 @@
+//without using method
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       var x = 75.86f;
+       var y = 43.48f;
+       var m = x + y;
+       Console.WriteLine(m);
+    }
+}
