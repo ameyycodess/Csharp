38a7e2b41119ebe101871f207d5f781d5e71d0f5
Amey Murkar <146580786+ameyycodess@users.noreply.github.com>
2024-07-03 23:37:20 +0530

    Add files via upload

--- Sum.cs
+++ Sum.cs
@@ -1,13 +1,14 @@
 using System;
 
-public class Sum
+public class HelloWorld
 {
     public static void Main(string[] args)
     {
         int m = 100;
         long n = 200;
-        long l = m + n;
-        int l1 = (int)l;
+      //int l = m + n; Cannot implicitly convert type 'long' to 'int'. An explicit conversion exists (are you missing a cast?)
+        long l = m + n; //provides correct o/p
+        int l1 = (int)l;//if we comment this line still the code will provide correct o/p
         Console.WriteLine(l1);
     }
 }
